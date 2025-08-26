dd137f203fe84279c4032a9173f94db53fd471c7
bookhabit <qnrgoqlt@naver.com>
2025-08-26 23:27:38 +0900

    feat: Add signing configuration for release builds and update .gitignore to include keystore file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../my-release-key.keystore")
+            storePassword = "ho08270827!"
+            keyAlias = "my-key-alias"
+            keyPassword = "ho08270827!"
+        }
+    }
+    
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -25,6 +34,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
